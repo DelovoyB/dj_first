59367757b97cc4da956c09dada128cd063cdbe5b
DanilaB <vdirectdanilu@gmail.com>
2024-05-28 17:41:19 +0300

    Catalog and product pages. Goods app. Right before DB

--- dj/urls.py
+++ dj/urls.py
@@ -20,4 +20,5 @@ from django.urls import include,path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
+    path('catalog/', include('goods.urls', namespace='catalog'))
 ]
