1260e13ff1ec2234e9275a6a900c1dab3c09cf38
DanilaB <vdirectdanilu@gmail.com>
2024-06-05 21:24:07 +0300

    db init

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=150,unique=True,verbose_name='Название')
+    slug = models.SlugField(max_length=200,unique=True,blank=True,null=True,verbose_name='URL')
+    
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'категорию'
+        verbose_name_plural = 'Категории'
