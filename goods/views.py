4133c784e894f0928b74d4b86b45608387c49845
DanilaB <vdirectdanilu@gmail.com>
2024-06-17 17:07:26 +0300

    pagination v1

--- goods/views.py
+++ goods/views.py
@@ -1,16 +1,26 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_list_or_404
 
-from goods.models import Products
+from goods.models import Products, Categories
+
 
 # Create your views here.
 
-def catalog(request):
+def catalog(request, category_slug, page=1):
 
-    goods = Products.objects.all()
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        category = Categories.objects.get(slug=category_slug)
+        goods = get_list_or_404(Products.objects.filter(category=category))
+
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(page)
 
     context = {
         'title':'Каталог',
-        'goods':goods,
+        'goods':current_page,
+        'slug_url':category_slug,
     }
     return render(request, 'goods/catalog.html', context)
 
