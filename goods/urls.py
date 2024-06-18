e0e8cdfead76d5260713b7fd490584dac49e253d
DanilaB <vdirectdanilu@gmail.com>
2024-06-18 18:41:08 +0300

    search v1

--- goods/urls.py
+++ goods/urls.py
@@ -3,6 +3,7 @@ from goods import views
 
 app_name = 'goods'
 urlpatterns = [
+    path('search/', views.catalog, name='search'),
     path('<slug:category_slug>/', views.catalog, name='index'),
     path('product/<slug:slug>', views.product, name='product'),
 ]
--- goods/views.py
+++ goods/views.py
@@ -2,19 +2,24 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_list_or_404
 
 from goods.models import Products, Categories
+from goods.utils import q_search
 
 
 # Create your views here.
 
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
 
     page = request.GET.get('page', 1)
 
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
 
+    query = request.GET.get('q', None)
+
     if category_slug == 'all':
         goods = Products.objects.all()
+    elif query:
+        goods = q_search(query)
     else:
         category = Categories.objects.get(slug=category_slug)
         goods = get_list_or_404(Products.objects.filter(category=category))
