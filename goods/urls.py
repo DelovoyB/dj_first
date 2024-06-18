732a16cf6e7192bcfaf6e96f8ed07c0f659cb0b3
Danila <65086743+DelovoyB@users.noreply.github.com>
2024-06-18 11:59:20 +0300

    Dev (#4)
    * .gitignore updated
    
    * some
    
    * Slug converter for products
    
    * pagination v1
    
    * pagination v2

--- goods/urls.py
+++ goods/urls.py
@@ -3,6 +3,6 @@ from goods import views
 
 app_name = 'goods'
 urlpatterns = [
-    path('', views.catalog, name='index'),
-    path('product/', views.product, name='product'),
+    path('<slug:category_slug>/', views.catalog, name='index'),
+    path('product/<slug:slug>', views.product, name='product'),
 ]
--- goods/views.py
+++ goods/views.py
@@ -1,18 +1,38 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_list_or_404
 
-from goods.models import Products
+from goods.models import Products, Categories
+
 
 # Create your views here.
 
-def catalog(request):
+def catalog(request, category_slug):
 
-    goods = Products.objects.all()
+    page = request.GET.get('page', 1)
+
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        category = Categories.objects.get(slug=category_slug)
+        goods = get_list_or_404(Products.objects.filter(category=category))
+
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(int(page))
 
     context = {
         'title':'Каталог',
-        'goods':goods,
+        'goods':current_page,
+        'slug_url':category_slug,
     }
     return render(request, 'goods/catalog.html', context)
 
-def product(request):
-    return render(request, 'goods/product.html')
+def product(request, slug):
+
+    product = Products.objects.get(slug=slug)
+
+    context = {
+        'title':product.name,
+        'product':product,
+    }
+
+    return render(request, 'goods/product.html', context=context)
