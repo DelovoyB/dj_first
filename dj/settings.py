aecdb1f4fe24f3b1196d7c4b1ee0ce8ee614eb6f
DanilaB <vdirectdanilu@gmail.com>
2024-06-24 00:37:29 +0300

    login form ready

--- dj/settings.py
+++ dj/settings.py
@@ -143,3 +143,5 @@ INTERNAL_IPS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
