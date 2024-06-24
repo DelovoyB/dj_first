6caecffd64509fa42c4883a31a73a403fd83939d
DanilaB <vdirectdanilu@gmail.com>
2024-06-25 01:24:59 +0300

    feat: registration ready

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from users.models import User
 
@@ -23,3 +23,18 @@ class UserLoginForm(AuthenticationForm):
 
     class Meta:
         model = User
+        fields = ['username', 'password']
+
+
+class UserRegistrationForm(UserCreationForm):
+
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ('first_name','last_name','username', 'email', 'password1', 'password2')
--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 # Create your views here.
@@ -28,8 +28,20 @@ def login(request):
     return render(request, 'users/login.html', context)
 
 def registration(request):
+
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
+
     context = {
         'title' : 'Регистрация',
+        'form' : form,
     }
     return render(request, 'users/registration.html', context)
 
@@ -40,4 +52,6 @@ def profile(request):
     return render(request, 'users/profile.html', context)
 
 def logout(request):
-    ...
+
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('main:index'))
