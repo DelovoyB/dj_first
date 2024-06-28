a3db12fe8078da2d31e5acb129118a0f74eb89b6
DanilaB <vdirectdanilu@gmail.com>
2024-06-28 18:29:43 +0300

    feat: change cart ajax implementation
    Change cart ajax and json resopnse, all ajax done

--- carts/views.py
+++ carts/views.py
@@ -36,8 +36,28 @@ def cart_add(request):
     return JsonResponse(response_data)
 
 
-def cart_change(request, product_slug):
-    ...
+def cart_change(request):
+
+    cart_id = request.POST.get("cart_id")
+    quantity = request.POST.get("quantity")
+
+    cart = Cart.objects.get(id=cart_id)
+
+    cart.quantity = quantity
+    cart.save()
+
+    cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        'carts/includes/included_cart.html', {"carts": cart}, request=request
+    )
+
+    response_data = {
+        "message": "Количество изменено",
+        "cart_items_html": cart_items_html,
+    }
+
+    return JsonResponse(response_data)
+
 
 
 def cart_remove(request):
