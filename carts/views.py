0d3de536add0e92416e710c570aeb080f917e7af
DanilaB <vdirectdanilu@gmail.com>
2024-06-28 10:55:09 +0300

    feat: cart remove
    Cart remove controller added, commit before ajax implementation

--- carts/views.py
+++ carts/views.py
@@ -21,9 +21,14 @@ def cart_add(request, product_slug):
 
     return redirect(request.META['HTTP_REFERER'])
 
+
 def cart_change(request, product_slug):
     ...
 
 
-def cart_remove(request, product_slug):
-    ...
+def cart_remove(request, cart_id):
+
+    cart = Cart.objects.get(id=cart_id)
+    cart.delete()
+
+    return redirect(request.META['HTTP_REFERER'])
