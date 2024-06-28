520e7ab023a347a1ed708b03c1a933674dc5fb61
DanilaB <vdirectdanilu@gmail.com>
2024-06-28 19:49:12 +0300

    feat: sessions
    Sessions - carts for ananymous user

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
+from carts.models import Cart
 from users.decorators import anonymous_required
 from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
@@ -20,9 +21,15 @@ def login(request):
             username = request.POST['username']
             password = request.POST['password']
             user = auth.authenticate(username=username, password=password)
+
+            session_key = request.session.session_key
+
             if user:
                 auth.login(request, user)
                 messages.success(request, f'Вы успешно вошли в аккаунт {username}')
+
+                if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
 
                 redirect_page = request.POST.get('next', None)
                 if redirect_page and redirect_page != reverse('user:logout'):
@@ -46,8 +53,15 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+
+            session_key = request.session.session_key
+
             user = form.instance
             auth.login(request, user)
+
+            if session_key:
+                Cart.objects.filter(session_key=session_key).update(user=user)
+
             messages.success(request, f'Вы успешно зарегистрировались {user.username}')
             return HttpResponseRedirect(reverse('main:index'))
     else:
