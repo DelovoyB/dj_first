c23d8036fba12a6d08415402f67231c939bdab59
DanilaB <vdirectdanilu@gmail.com>
2024-07-01 16:39:21 +0300

    feat: order app init
    Created order template and fomr prepared

--- users/models.py
+++ users/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class User(AbstractUser):
 
     image = models.ImageField(upload_to='users_images', blank=True, null=True, verbose_name='Изображение')
+    phone_number = models.CharField(max_length=15, blank=True, null=True, verbose_name='Номер телефона')
 
     class Meta:
         db_table = 'user'
