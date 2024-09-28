5a9d9e98fe0b2fe6a10e93063d7955743fde111c
DanilaB <vdirectdanilu@gmail.com>
2024-09-28 18:01:24 +0300

    feat: new api app implementation
    Viewsets, routers and authentication implementation in Api app to reach
    endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,17 +1,15 @@
-from django.urls import path
-from api import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from api.views import ProductViewSet, UserViewSet, CategoryViewSet, OrderViewSet
 
 app_name = 'api'
+
+router = DefaultRouter()
+router.register('users', UserViewSet)
+router.register('categories', CategoryViewSet)
+router.register('products', ProductViewSet)
+router.register('orders', OrderViewSet)
+
 urlpatterns = [
-    path('users/all/', views.UsersList.as_view(), name='users_list'),
-    path('users/<str:username>/', views.UsersRetrieveUpdateDestroy.as_view(), name='users_retrieve_update_destroy'),
-
-    path('categories/all/', views.CategoriesList.as_view(), name='categories_list'),
-    path('categories/<str:slug>/', views.CategoriesRetrieveUpdateDestroy.as_view(), name='categories_retrieve_update_destroy'),
-
-    path('products/all/', views.ProductsList.as_view(), name='products_list'),
-    path('products/<str:slug>/', views.ProductsRetrieveUpdateDestroy.as_view(), name='products_retrieve_update_destroy'),
-
-    path('orders/all/', views.OrdersList.as_view(), name='orders_list'),
-    path('orders/<int:id>/', views.OrdersRetrieveUpdateDestroy.as_view(), name='orders_retrieve_update_destroy'),
+    path('', include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
 from api.serializer import UserSerializer, CategorySerializer, ProductSerializer, OrderSerializer
 from goods.models import Categories, Products
 from orders.models import Order
@@ -8,49 +11,42 @@ from users.models import User
 
 
 @method_decorator(cache_page(30), name='dispatch')
-class UsersList(generics.ListCreateAPIView):
+class UserViewSet(viewsets.ModelViewSet):
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsAuthenticated, IsAdminUser]
+        return [permission() for permission in permission_classes]
 
-
-class UsersRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    lookup_field = 'username'
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if not self.request.user.is_staff:
+            queryset = queryset.none()
+        return queryset
 
 
 @method_decorator(cache_page(30), name='dispatch')
-class CategoriesList(generics.ListCreateAPIView):
-    queryset = Categories.objects.all()
-    serializer_class = CategorySerializer
-
-
-class CategoriesRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Categories.objects.all()
     serializer_class = CategorySerializer
     lookup_field = 'slug'
 
 
 @method_decorator(cache_page(30), name='dispatch')
-class ProductsList(generics.ListCreateAPIView):
-    queryset = Products.objects.all()
-    serializer_class = ProductSerializer
-
-
-class ProductsRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+class ProductViewSet(viewsets.ModelViewSet):
     queryset = Products.objects.all()
     serializer_class = ProductSerializer
     lookup_field = 'slug'
 
 
 @method_decorator(cache_page(30), name='dispatch')
-class OrdersList(generics.ListCreateAPIView):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
-
-
-class OrdersRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     lookup_field = 'id'
